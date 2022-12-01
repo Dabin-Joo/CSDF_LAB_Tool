d87b457b470c0fb4b920b69d2c49bff81365a771
BeenyHail <119034536+BeenyHail@users.noreply.github.com>
2022-12-01 17:31:21 +0900

    Update logging.py

--- PWB/Mac_Forensic_tool/logging.py
+++ PWB/Mac_Forensic_tool/logging.py
@@ -1,14 +1,13 @@
-#log show 명령어 사용해서 콘솔 출력 없이 사용
+#Using 'log show' command without password
 import os
 
 def getLog():
     stream = os.popen('log show')
     output = stream.readlines()
-    #for i in range(0,10):
-    #    print(output[i])
+
     now_path = os.getcwd()
     file = now_path+"/log.txt"
-    with open(file, "w") as f: #현재 경로에 다른 파일(log.txt)로 저장 -> 이 과정에서의 데이터 무결성은 어떻게 확인?
+    with open(file, "w") as f: 
         for i in range(0, len(output)):
             f.write(output[i])
     print("Check the file, 'log.txt'")
