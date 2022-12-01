0f94be9f0578b8b783c760886e1d1ee34c807808
BeenyHail <119034536+BeenyHail@users.noreply.github.com>
2022-12-01 17:30:18 +0900

    Update install_list.py

--- PWB/Mac_Forensic_tool/install_list.py
+++ PWB/Mac_Forensic_tool/install_list.py
@@ -1,15 +1,16 @@
-#설치된 어플리케이션의 이름을 나열
+#lists the installed applications' name
 import sqlite3
 import os
+import time
 
 def check_program():
-    #knowledgeC.db 접근
     username = input("Input user's name: ")
     now_path = os.getcwd()
     k_path = "/Users/"+username+"/Library/Application Support/Knowledge"
     os.chdir(k_path)
     com = "cp knowledgeC.db "+now_path
     os.popen(com)
+    time.sleep(3)
 
     os.chdir(now_path)
     con = sqlite3.connect('knowledgeC.db')
@@ -23,6 +24,3 @@ def check_program():
             if j not in program:
                 program.append(j)
     print(program)
-
-    
-    
