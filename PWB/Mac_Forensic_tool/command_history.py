0b94b518b13a2a47d5f9643e138d6bab6623dbe6
BeenyHail <119034536+BeenyHail@users.noreply.github.com>
2022-12-01 17:25:52 +0900

    Update command_history.py

--- PWB/Mac_Forensic_tool/command_history.py
+++ PWB/Mac_Forensic_tool/command_history.py
@@ -1,13 +1,13 @@
-#bash_history or zsh_history 분석 파일 -> 완
+#bash_history or zsh_history 
 import os
 
 def print_exCommand():
     username = input("Input user's name: ")
     
+    #check the os version
     lines = os.popen("sw_vers")
     for line in lines:
         list1 = line.split(":")
-        #print(list1)
         if list1[0] == "ProductVersion":
             os_version = list1[1]
             os_version = os_version.replace("\t", "")
@@ -15,13 +15,14 @@ def print_exCommand():
             os_list = os_version.split(".")
             for idx, i in enumerate(os_list):
                 os_list[idx] = int(i)
-    #mac 기본 쉘 타입 = 10.15 미만은 bash, 10.15 이상 zsh 기본 사용
+                
+    #mac shell type -> 10.14 or lower : bash, 10.15 or higher : zsh
     if os_list[0] < 10:
         history_path = "/Users/"+username+"/.bash_history"
     elif os_list[0] ==10 and os_list[1]<15:
         history_path = "/Users/"+username+"/.bash_history"
     else:
-        history_path = "/Users/"+username+"/.zsh_history" #데이터 무결성 확인 방법
+        history_path = "/Users/"+username+"/.zsh_history"
 
     with open(history_path, "rb") as file:
         lines = file.readlines()
